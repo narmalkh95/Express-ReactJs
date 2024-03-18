9c50b87b6424dffd906224da27e1c419b21d8926
Artak Kostanyan <artak.kostanyan@mentorcliq.com>
2024-03-18 18:22:46 +0400

    reomve logs

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './ErrorPage.module.css';
 
 const ErrorPage = () => {
-
+     const location = useLocation();
+     const errorMessage = new URLSearchParams(location.search).get('error');
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Դուք առայժմ դասեր չունեք!</h1>
+            <h1 className={styles.title}>{errorMessage || "Դուք առայժմ դասեր չունեք!"}</h1>
         </div>
     );
 };
